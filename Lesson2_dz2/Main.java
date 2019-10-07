91d48acdbb81f50835102c9fa7dbf338726ced1c
mbakhmetiev <mbakhmetiev@yandex.ru>
2019-10-07 20:17:25 +0300

    Create Main.java

---
+++ Lesson2_dz2/Main.java
@@ -0,0 +1,21 @@
+package Lesson2_dz2;
+
+public class Main {
+    public static void main(String[] args) {
+        SumArray sumArray = new SumArray();
+
+        String[][][] stringArray = {{{"1", "2", "3", "4"}, {"5", "6", "7", "8"}, {"9", "10", "11", "12"}},
+                {{"1", "2", "3", "4"}, {"5", "6", "*", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "HELLO"}},
+                {{"1", "2", "3", "4"}, {"5", "6", "7", "8"}, {"9", "10", "11", "12"}, {"13", "14", "15", "16"}}};
+
+        for (int i = 0; i < stringArray.length; ++i) {
+            try {
+                System.out.printf("Сумма элементов массива: %d%n",sumArray.setSumArray(4, stringArray[i]));
+            } catch (MyArraySizeException ex) {
+                System.err.println(ex.getMessage());
+            } catch (MyArrayDataException ex) {
+                System.err.println(ex.getMessage());
+            }
+        }
+    }
+}
